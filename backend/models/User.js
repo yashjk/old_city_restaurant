7bce5f8a6b5bb1a35b49845861e397b226435009
yash <injose.joshi@gmail.com>
2024-09-06 18:04:43 +0530

    Add user schema

---
+++ backend/models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema(
+	{
+		firstName: { type: String, required: true },
+		lastName: { type: String, required: true },
+		email: { type: String, required: true, unique: true },
+		password: {
+			type: String,
+			required: function () {
+				return this.type === "authenticated";
+			},
+		},
+		type: {
+			type: String,
+			enum: ["authenticated", "guest"], // Only allow two types of users
+			required: true,
+			default: "guest",
+		},
+		reservations: [
+			{ type: mongoose.Schema.Types.ObjectId, ref: "Reservation" },
+		],
+		orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
+		reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
+	},
+	{ timestamps: true }
+);
+
+// Hash the password before saving the user
+UserSchema.pre("save", async function (next) {
+	const user = this;
+
+	if (user.isModified("password") && user.type === "authenticated") {
+		const salt = await bcrypt.genSalt(10);
+		user.password = await bcrypt.hash(user.password, salt);
+	}
+	next();
+});
+
+// Method to compare hashed passwords (used only for "authenticated" users)
+UserSchema.methods.comparePassword = async function (enteredPassword) {
+	if (this.type === "guest") return false; // Guests don't have a password
+	return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
