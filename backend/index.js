03cf4faec427da4aa055ce85a184ffa9c73b796e
yash <injose.joshi@gmail.com>
2024-09-18 14:15:49 +0530

    Add cosmetic refactoring

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ const dotenv = require("dotenv");
 // Routes
 const authRoute = require("./src/routes/auth");
 const userRoute = require("./src/routes/user");
+const menuItemRoute = require("./src/routes/menuItem");
+const orderRoute = require("./src/routes/order");
 
 dotenv.config();
 
@@ -18,6 +20,8 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/menuItems", menuItemRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5001, () => {
 	console.log("Backend server is running on port 5001");
