6976c1ad4e1188028f729a6f52854297e0def452
yash <injose.joshi@gmail.com>
2024-09-18 14:43:09 +0530

    Add reservation routes and controller

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const authRoute = require("./src/routes/auth");
 const userRoute = require("./src/routes/user");
 const menuItemRoute = require("./src/routes/menuItem");
 const orderRoute = require("./src/routes/order");
+const reservationRoute = require("./src/routes/reservation");
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/menuItems", menuItemRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/reservations", reservationRoute);
 
 app.listen(process.env.PORT || 5001, () => {
 	console.log("Backend server is running on port 5001");
