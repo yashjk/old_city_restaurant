77780fad6fdfe8702e472eb47443429f0c9e6997
yash <injose.joshi@gmail.com>
2024-09-21 16:57:42 +0530

    Add cosmetic refactoring

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
-import AboutUs from "./components/AboutUs";
-import Footer from "./components/Footer";
-import Gallery from "./components/Gallery";
-import Header from "./components/Header";
-import Offers from "./components/Offers";
+import AboutUs from "../components/AboutUs";
+import Footer from "../components/Footer";
+import Gallery from "../components/Gallery";
+import Header from "../components/Header";
+import Offers from "../components/Offers";
 
 export default function Home() {
   return (
